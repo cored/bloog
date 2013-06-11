b99f154b1fdc1be9cbf52cff5f58903a083a0c7e
Rafael George <george.rafael@gmail.com>
2013-06-11 18:40:43 -0400

    Initial commit with a blog controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloog::Application.config.secret_token = 'b9b6812be6abcf91a9cab497efa647d85e2a6a507471b5e7b59a647ec1250f50872fba5b8c5aaa965a282f9e4a320a1d6ee29debc60eae735259c683d5de5a0c'
