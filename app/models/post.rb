1724183dfd25025a5e7644d115e3b87a6b20c8e9
Rafael George <george.rafael@gmail.com>
2013-06-21 15:40:46 -0400

    Remove all ActiveModel dependant stuff seems now Post will inherance behavior from ActiveRecord::Base directly

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,25 +1,15 @@
 require 'date'
+require 'active_record'
 
-class Post
-  extend ActiveModel::Naming
-  include ActiveModel::Conversion
-  include ActiveModel::Validations
+class Post < ActiveRecord::Base
   validates :title, presence: true
 
-  attr_accessor :blog, :title, :body, :pubdate, :image_url
-
-  def initialize(attrs={})
-    attrs.each do |k,v| send("#{k}=",v) end
-  end
+  attr_accessor :blog
 
   def publish(clock=DateTime)
     return false unless valid?
     self.pubdate = clock.now
     blog.add_entry(self)
-  end
-
-  def persisted?
-    false
   end
 
   def picture?
